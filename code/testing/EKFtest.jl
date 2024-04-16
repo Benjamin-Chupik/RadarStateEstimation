5e5ffefe45ba894b25d9c3a602f39940f719d278
GuidoInsinger <79085571+GuidoInsinger@users.noreply.github.com>
2024-04-16 12:06:51 -0600

    EKF update

--- code/testing/EKFtest.jl
+++ code/testing/EKFtest.jl
@@ -12,41 +12,31 @@ using Plots
 # Dynamics
 Cd = 0.1
 params = Params(.5, 100.0, Cd, .2)
-x0 = [0.0, 50.0, 0.4, 15.0]
+x0 = [50.0, 50.0, 0.3, 20.0]
 x_list, u_list = genTrajectory(x0, params)
 
 # Measurements
-function rNoise()
-    return 0.0
-    # return rand(Chisq(4))
-end
-function rdNoise()
-    return 0.0
-    #return rand(Normal(0,.2))
-end
-function azNoise()
-    return 0.0
-    # return rand(Normal(0.0, deg2rad(2)))
-end
-function elNoise()
-    return 0.0
-    # return rand(Normal(0.0, deg2rad(2)))
-end
-radar = Radar([50.0,0.0], rNoise, rdNoise, azNoise, elNoise)
+rNoise = Normal(0,.2) # Chisq(4)
+rdNoise = Normal(0,.2)
+elNoise = Normal(0.0, deg2rad(2))
+
+radar = Radar([50.0,0.0], rNoise, rdNoise, elNoise)
+
+
 y_list = radarMeasure(x_list, radar)
 
 # init P
-bignum = Diagonal([100, 100, 1, 50])
-P0 = ones(4, 4)*bignum
+P0 = Diagonal([10, 10, 0.2, 5])
 
 # init Qk, Rk
-Qk = Diagonal([5, 5, 0.1, 5])
-Rk = Diagonal([10, 10, 0.3])
+Qk = Diagonal([0.01, 0.01, 0.005, 0.01])
+@show Rk = Diagonal([var(rNoise), var(rdNoise), var(elNoise)])
 
 
 # print(length(y_list))
 # print(length(u_list))
-x_EKF, P_EKF = EKF_bulk(x0*1.3, P0, u_list, y_list, Qk, Rk, params, radar)
+@show x_init = rand(MvNormal(x0, P0))
+x_EKF, P_EKF = EKF_bulk(x_init, P0, u_list, y_list, Qk, Rk, params, radar)
 
 x_EKF = stack(x_EKF, dims=1)
 xMat = stack(x_list, dims=1)
