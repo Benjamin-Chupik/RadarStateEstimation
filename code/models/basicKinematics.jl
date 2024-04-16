9e15ebb808d7121f4a084090b676d073a6018219
BHChupik <BHChupik@gmail.com>
2024-04-15 19:50:05 -0600

    Remove uneeded function definition

--- code/models/basicKinematics.jl
+++ code/models/basicKinematics.jl
@@ -3,16 +3,6 @@ This file hold basic kinematic models.
 Manily used for estimators
 """
 
-function linearDynamics(xk::Vector{Float64}, params::Params)
-    α  = xk[3]
-    v = xk[4]
-    A = [0 0 -v*sin(α) cos(α)
-         0 0  v*cos(α) sin(α)
-         0 0 0 0
-         0 0 0 -2*v*Cd/params.ρ]
-    return A
-end
-
 function simulate(xk::Vector{Float64}, params::Params)
     """
     Simulates the dynamics simulate the dynamics one k forward in time using numeric integration. 
